4281c3279a3664b1c83263b41c5b022e1b3d938b
Marc Bischof <marc@bischof.io>
2025-09-18 18:13:26 +0200

    Add a task template

--- source/isaac_lab_ur10e_sim2real_tasks/isaac_lab_ur10e_sim2real_tasks/tasks/manager_based/template/__init__.py
+++ source/isaac_lab_ur10e_sim2real_tasks/isaac_lab_ur10e_sim2real_tasks/tasks/manager_based/template/__init__.py
@@ -17,7 +17,7 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.isaac_lab_ur10e_sim2real_tasks_env_cfg:IsaacLabUr10eSim2realTasksEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.template_env_cfg:IsaacLabUr10eSim2realTasksEnvCfg",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
         "skrl_amp_cfg_entry_point": f"{agents.__name__}:skrl_amp_cfg.yaml",
