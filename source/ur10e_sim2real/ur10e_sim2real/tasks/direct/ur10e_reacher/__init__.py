46717dfcb85cd42900342d8135cf6b1fadb02470
Marc Bischof <marc@bischof.io>
2025-09-19 11:46:19 +0200

    Update init

--- source/ur10e_sim2real/ur10e_sim2real/tasks/direct/ur10e_reacher/__init__.py
+++ source/ur10e_sim2real/ur10e_sim2real/tasks/direct/ur10e_reacher/__init__.py
@@ -18,7 +18,6 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": f"{__name__}.ur10e_reacher_env_cfg:UR10eReacherEnvCfg",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_cfg_ur10e_reacher.yaml",
     },
 )
